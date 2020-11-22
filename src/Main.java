1ccf5ac418a6ae81d3eaf1c7e4f87d86d1f43a61
unknown <katrinkuzukovich@gmail.com>
2020-11-22 22:40:31 +0300

    creat object

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Dog dog=new Dog();
+        Plane plane=new Plane();
+        Restaurant restaurant=new Restaurant();
+        Shop shop=new Shop();
+        Phone phone=new Phone();
+
 
     }
 }
