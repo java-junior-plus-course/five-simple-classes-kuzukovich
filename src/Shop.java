28ed44f5b5b9e90f657de66ad0340d7b20d95374
unknown <katrinkuzukovich@gmail.com>
2020-11-22 22:06:23 +0300

    add methods Shop

--- src/Shop.java
+++ src/Shop.java
@@ -3,4 +3,20 @@ public class Shop {
   public String  address;
   public Boolean parking;
   public int phone;
+
+  public void sell(){
+
+  }
+    public void buy(){
+
+    }
+    public void serve(){
+
+    }
+    public void pay(){
+
+    }
+    public void advertise(){
+
+    }
 }
