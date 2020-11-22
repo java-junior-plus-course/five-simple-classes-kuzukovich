e4be932b5c30e3fddb8dfc9a5bd9c82aac3735b7
unknown <katrinkuzukovich@gmail.com>
2020-11-22 21:56:43 +0300

    add methods Dog

--- src/Dog.java
+++ src/Dog.java
@@ -3,4 +3,21 @@ public class Dog {
     private String color;
     public int age;
     public String gender;
+
+    public void run(){
+
+    }
+    public void bark() {
+
+    }
+    public void eat() {
+
+    }
+    public void stop() {
+
+    }
+    public void lay() {
+
+    }
+
 }
